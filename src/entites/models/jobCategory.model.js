06a789d47de5589d92f25f1d7a62611ec065fc91
hadhi hassan m <diluhadhi01@gmail.com>
2024-02-14 01:00:36 -0800

    validation completed

---
+++ src/entites/models/jobCategory.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const JobCategorySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 10
+    },
+    description: {
+        type: String,
+        required: true,
+        minlength: 10,
+        maxlength: 100,
+    },
+    icons: {
+        type: String, 
+        required: true
+    },
+    workingBelow: {
+        type: Number, 
+        default: 0
+    },
+    status: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+const JobCategory = mongoose.model("Job_Category", JobCategorySchema); 
+export default JobCategory;
