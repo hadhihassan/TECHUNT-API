c29d6a1c34bb0e68fd8b968ee02d5e133150d635
hadhi hassan m <diluhadhi01@gmail.com>
2024-04-16 07:04:47 -0700

    bugfix: bcrypt moduel bug fix reinstalled .

--- src/providers/bcryptPassword.js
+++ src/providers/bcryptPassword.js
@@ -2,18 +2,15 @@ import bcrypt from 'bcrypt'
 import { SALT_ROUND } from '../constants/constant.js'
 
 export class Encrypt {
-    //Hashing the password for the secure.
     async encryptPassword(password) {
         const salt = await bcrypt.genSalt(SALT_ROUND);
         return await bcrypt.hash(password, salt)
     }
-    //To compare two password .
     async comparePasswords(password, hashedPassword) {
         try {
             const result = await bcrypt.compare(password, hashedPassword);
             return result;
         } catch (error) {
-            console.error(error);
             throw error; 
         }
     }   
