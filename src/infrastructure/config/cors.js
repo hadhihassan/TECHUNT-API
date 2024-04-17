c168c29b8cbc6db0859c722c94e4a1d1fe04965f
hadhi hassan m <diluhadhi01@gmail.com>
2024-04-17 03:22:13 -0700

    feat: production ready some changes makes..

--- src/infrastructure/config/cors.js
+++ src/infrastructure/config/cors.js
@@ -1,8 +1,7 @@
 import cors from "cors";
 
-// origin: 'https://techunt-7l0ushpny-hadhi-hassan-s-projects.vercel.app/',
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://techunt.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204,
